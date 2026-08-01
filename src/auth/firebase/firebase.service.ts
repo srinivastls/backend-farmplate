c1a210fc69cb87dfcf7d2d46647ab9d38db97014
srinivastls <lakshmisrinivas365@gmail.com>
2026-08-01 23:56:57 +0530

    Use Firebase env variables

--- src/auth/firebase/firebase.service.ts
+++ src/auth/firebase/firebase.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import * as admin from 'firebase-admin';
-import * as path from 'path';
-
-const serviceAccount = require(
-  path.join(process.cwd(), 'firebase-service-account.json'),
-);
 
 @Injectable()
 export class FirebaseService {
   constructor() {
     if (!admin.apps.length) {
       admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
+        credential: admin.credential.cert({
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        }),
       });
     }
   }
